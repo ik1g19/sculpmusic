20498fa6f4f8945e1bea9dbe1cebc7338f4e2cdb
isaacklugman@gmail.com <ik1g19@soton.ac.uk>
2022-03-27 12:58:28 +0100

    all gone wrong

--- Scripts/Storylet.cs
+++ Scripts/Storylet.cs
@@ -48,7 +48,23 @@ public class Storylet : MonoBehaviour
 
 
     public void OnMouseDown() {
-        //tapePlayer.insert(tape);
-        if (available) play();
+        // //tapePlayer.insert(tape);
+        // if (available) play();
+
+        // Create the synthesizer.
+        // var sampleRate = 44100;
+        // var synthesizer = new Synthesizer("TimGM6mb.sf2", sampleRate);
+
+        // // Read the MIDI file.
+        // var midiFile = new MidiFile("flourish.mid");
+        // var sequencer = new MidiFileSequencer(synthesizer);
+        // sequencer.Play(midiFile, false);
+
+        // // The output buffer.
+        // var left = new float[(int)(sampleRate * midiFile.Length.TotalSeconds)];
+        // var right = new float[(int)(sampleRate * midiFile.Length.TotalSeconds)];
+
+        // // Render the waveform.
+        // sequencer.Render(left, right);
     }
 }
